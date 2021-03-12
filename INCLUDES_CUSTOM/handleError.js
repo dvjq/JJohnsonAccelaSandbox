c6076c4a866b33057ef1ecfb94f7460eeef2fa96
JeremiahJohnson <jjohnson@accela.com>
2021-03-12 13:49:51 -0700

    updates

--- INCLUDES_CUSTOM/handleError.js
+++ INCLUDES_CUSTOM/handleError.js
@@ -2,8 +2,6 @@ function handleError(err, context) {
 	var rollBack = true;
 	var showError = true;
 
-	//TODO: Cut this line if you put your AgencyMonitor in INCLUDES_CUSTOM
-	eval(getScriptText("AgencyMonitor"));
 	//NOTE: This line is just here so handleError doesn't depend on your config choice to put AgencyMonitor on its own or in INCLUDES_CUSTOM.
 	var aMon = new AgencyMonitor();
 
@@ -13,15 +11,9 @@ function handleError(err, context) {
 	logDebug("Stack: " + err.stack);
 
 	if (aMon) {  
-		//TODO: Select only either the slack or email section below unless you want to use both. 
 		//for error slack
 		if (lookup("EventExceptionLog", "ErrorChannel")) {
 			aMon.error(err, context, capId);
-		}
-
-		//for error email
-		if (lookup("EventExceptionLog", "sendTo")) {
-			aMon.errorEmail(capId, err, context);
 		}
 	}
 }
